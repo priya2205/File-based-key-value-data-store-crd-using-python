b1b4ae14bf2cc8ccd11efce8bd372e568b29f5d3
priya <47935577+priya2205@users.noreply.github.com>
2020-12-01 22:12:38 +0530

    Add files via upload

---
+++ dataStore.py
@@ -0,0 +1,177 @@
+import json
+from threading import Lock
+import time
+import os
+# import math 
+lst = []
+class DataStore:
+    
+    def __init__(self,filename):
+        self.c=0
+        self.filename = filename
+        
+        
+        # isFile = os.path.isfile(filename)
+        
+        if filename not in lst:
+           self.fn =str(filename)+".json" 
+           self.fptr = open(self.fn,"w+")
+           lst.append(filename)
+        else:
+           print("Oops!,The file already exists and you cannot acceess it,To remove this error Please Create a new file")
+           return 
+        # dct={}
+        # with open(self.fn,"w") as json_file:
+        #     json_file.writelines('\n'.join(dct))
+        self.lock = Lock()
+    #create(key,value,timetolive_in_sec) - syntax 
+    def create(self,key,value,timetolive=0):
+        self.c+=1
+        dataDict = {}
+        fp = open(self.fn,'r')
+        self.lock.acquire()
+        if self.c>1:
+            with open(self.fn,"r") as json_file:
+                dataDict = json.load(json_file)
+        if key in dataDict:
+            print("Oops!it seems to be the Key exists already, enter a new key")# error msg
+        else:
+                if(key.isalnum()):
+                      if (len(dataDict)+fp.tell())<(1024*1024*1024) and value<=(16*1024*1024):
+                          if timetolive==0:
+                                  l = {'value':value,'timetolive':timetolive}
+                          else:
+                                  l ={'value':value,'timetolive':time.time()+timetolive}
+                          if len(key)<=32:
+                                dataDict[key]=l
+                                
+                                json_data = json.dumps(dataDict,indent=4)
+                                if self.c==1:
+                                    with open(self.fn,"w") as outfile:
+                                        #print(json_data)
+                                        outfile.write(json_data)
+                                if self.c>1:
+                                    with open(self.fn,"r") as json_file:
+                                         d = json.load(json_file)
+                                         d[key]=l
+                                    with open(self.fn,"w") as json_file:
+                                         json.dump(d,json_file)
+                                # with open(self.fn,"w") as outfile:
+                                   
+                                #     outfile.writelines('\n'.join(d))
+                                
+                          else:
+                                print("OOPs! The key exceeds more than 32 char, the key must be capped at 32 chars")
+                      elif value>(16*1024*1024):
+                             print("OOPs! The value exceeds more than 16kb, the key must be capped at  16kb")
+                      else:
+                             print("Sorry! Memory limit has been exceeded")
+                else:
+                       print("OOPs! Your Key Name is Invalid, Key name can contain numbers(0-9) and alphabets(a-z A-Z) but no special characters") 
+
+        self.lock.release()
+    def read(self,key):
+      resdata=''
+      self.lock.acquire()
+      with open(self.fn,"r") as json_file:
+           data = json.load(json_file)
+      if key not in data:
+         print("Oops ! The key you entered is not available, Please ensure you entered a Valid Key")
+      else:
+         t = data[key]
+         if t['timetolive']!=0:
+            if time.time()<t['timetolive']:
+               
+                resdata = str(key)+':'+'['+str(t['value'])+','+str(t['timetolive'])+']'
+                print("The data is for the key"+"'"+str(key) +"'"+resdata)
+            else:
+                print("Sorry,The Key you entered "+str(key)+" its Time to live has been expired "+str(round(time.time()-t['timetolive'],4))+"Seconds ago")
+         else:
+                resdata = str(key)+':'+"["+str(t['value'])+','+str(t['timetolive']) +"]"
+                print("The data is for the key"+"'"+str(key) +"'"+resdata)
+      self.lock.release()
+               
+    def delete(self,key):
+         self.lock.acquire()
+         with open(self.fn,"r") as json_file:
+           data = json.load(json_file)
+         json_line=[]
+         if key not in data:
+             print("Oops ! The key you entered is not available, Please ensure you entered a Valid Key")
+         else:
+             t = data[key]
+             if t['timetolive']!=0:
+                 if t['timetolive']<time.time():
+                     with open(self.fn,"r") as json_file:
+                            d = json.load(json_file)
+                     del d[key]
+                     with open(self.fn,"w") as json_file:
+                         json.dump(d,json_file)
+                     print("Successful!, The key "+str(key)+" is deleted")
+                 else:
+                     print("Sorry,The Key you entered"+str(key)+" its Time to live has been expired "+str(round(time.time()-t['timetolive'],4))+"Seconds ago")
+ 
+             else:
+                 with open(self.fn,"r") as json_file:
+                            d = json.load(json_file)
+                 del d[key]
+                 with open(self.fn,"w") as json_file:
+                     json.dump(d,json_file)
+                 print("Successful!, The key "+str(key)+" is deleted")
+         self.lock.release()
+#modifyValue(key,newvalu e)
+    def modifyValue(self,key,value):
+      self.lock.acquire()
+      with open(self.fn,"r") as json_file:
+          data = json.load(json_file)
+      if key not in data:
+         print("Oops ! The key you entered is not available, Please ensure you entered a Valid Key")
+
+      else:
+             t = data[key]
+             if(key.isalnum()):
+                  if value<=(16*1024*1024):
+                         l = {'value':value,'timetolive':t['timetolive']} # add the timeto live value and new value 
+                         if len(key)<=32:
+                            with open(self.fn,"r") as json_file:
+                                 d = json.load(json_file)
+                            d[key] = l
+                            with open(self.fn,"w") as json_file:
+                                  json.dump(d,json_file)
+                         else:
+                             print("OOPs! The key exceeds more than 32 char, the key must be capped at 32 chars")
+                  else:
+                      print( "OOPs! The value exceeds more than 16kb, the key must be capped at 16kb")
+             else:
+                 print("OOPs! Your Key Name is Invalid, Key name can contain numbers(0-9) and alphabets(a-z A-Z) but no special characters") 
+      self.lock.release()
+    #extendTimeToLive(key,how_many_seconds_to_be_extended)
+    def extendTimeToLive(self,key,timetolive):
+         self.lock.acquire()
+         with open(self.fn) as json_file:
+           data = json.load(json_file)
+         if key not in data:
+             print("Oops ! The key you entered is not available, Please ensure you entered a Valid Key")
+         else:
+             t = data[key]
+             if t['timetolive']!=0:
+                
+                 if t['timetolive']>time.time():
+                      l = {'value':t['value'],'timetolive':t['timetolive']+timetolive}
+                      with open(self.fn,"r") as json_file:
+                             d = json.load(json_file)
+                      d[key] = l
+                      with open(self.fn,"w") as json_file:
+                           json.dump(d,json_file)
+                      print("Successful!, The key "+str(key)+" time to live extended")
+                 else:
+                     print("Sorry,The Key you entered "+str(key)+" its Time to live has been expired "+str(time.time()-t['timetolive'])+"Seconds ago")
+             else:
+                  l = {'value':t['value'],'timetolive':timetolive}
+                  with open(self.fn,"r") as json_file:
+                            d = json.load(json_file)
+                  d[key] = l
+                  with open(self.fn,"w") as json_file:
+                            json.dump(d,json_file)
+                  print("Successful!, The key "+str(key)+" timr to live extended")
+         self.lock.release()
---
+++ operations.py
@@ -0,0 +1,68 @@
+#here are the commands to demonstrate how to access and perform operations on a main file
+
+
+#run the MODULE of MAIN FILE and import mainfile as a library 
+
+import dataStore as crd 
+import threading
+from threading import*
+import time
+#importing the main file "dataStore" is the name of the file I have used as a library 
+
+ds = crd.DataStore('fourth')
+ds.create('priya',20)
+#to create a key with key_name,value given and with no time-to-live property
+ds.create("!priya_v",40)
+ds.create("eriujahhekjfhjhfsjhfdbfjdfkjdfbdjfhkdjeawegadjkadgadjad",802894)
+ds.create("soba",32)
+
+ds.create("madhu",30,3) 
+#to create a key with key_name,value given and with time-to-live property value given(number of seconds)
+ds.create("priya",20)
+#it returns an ERROR since the key_name already exists in the database
+ds.read("priya")
+#it returns the value of the respective key in Jasonobject format 'key_name:value'
+
+time.sleep(3)
+ds.read("madhu")
+#it returns the value of the respective key in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
+
+
+ds.create("priya",50)
+#it returns an ERROR since the key_name already exists in the database
+#To overcome this error 
+#either use modify operation to change the value of a key
+#or use delete operation and recreate it
+
+ds.create("elliot",28)
+#ds.modifyKey("elliot",24)
+ds.create("vandu",30,200)
+ 
+ds.extendTimeToLive("vandu",90)
+#it replaces the initial valueof timetolive of the respective key with new value of timetolive
+ds.read("vandu")
+ds.delete("elliot")
+#it deletes the respective key and its value from the database(memory is also freed)
+ds.modifyValue("vandu",40)
+#it replaces the initial value of the respective key with new value
+
+#we can access these using multiple threads like
+t1=Thread(target=(ds.create),args=('abc',200,10)) 
+t1.start()
+t2=Thread(target=(ds.create),args=('priyavandu',40,10))
+t2.start()
+t3=Thread(target=(ds.read),args=('priyavandu',)) 
+t3.start()
+
+#ds = crd.DataStore('third')
+dx = crd.DataStore('fourth')
+#this gives an error because there exists a file with this name already so not more than 1 person can acces a file at any time
+
+
+'''
+the code also returns other errors like 
+"invalidkey" if key_length is greater than 32 or key_name contains any numeric,special characters etc.,
+"key doesnot exist" if key_name was mis-spelt or deleted earlier
+"File memory limit reached" if file memory exceeds 1GB
+Key exists already
+'''
